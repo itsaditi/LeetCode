85d23bfef95de1e5c35269951b1eef12d99e3090
itsaditi <aditi3049@gmail.com>
2024-10-22 01:20:22 -0400

    Create file

---
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int product = 1;
+        int zeros = 0;
+        for (Integer num: nums) {
+            if (num != 0) {
+                product = product * num;
+            } else {
+                zeros++;
+            }
+        }
+
+        for (int i = 0 ; i < nums.length ; i ++) {
+            if (zeros >= 2) {
+                nums[i] = 0;
+            } else if (zeros == 1) {
+                if (nums[i] != 0) {
+                    nums[i] = 0;
+                } else {
+                    nums[i] = product;
+                }
+            } else {
+                nums[i] = product / nums[i];
+            }
+            
+        }
+
+        return nums;
+
+        
+    }
+}
