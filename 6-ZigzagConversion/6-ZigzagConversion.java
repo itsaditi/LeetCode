2da052043d676dcca8527ad65ce88ab7a8679ef9
itsaditi <aditi3049@gmail.com>
2024-10-25 22:20:56 -0400

    Update file 10/25/2024, 10:20:56 PM

--- 6-ZigzagConversion/6-ZigzagConversion.java
+++ 6-ZigzagConversion/6-ZigzagConversion.java
@@ -2,7 +2,7 @@ class Solution {
     public String convert(String s, int numRows) {
 
         if (s.length() == 1 || numRows == 1) return s;
-        
+
         int stringLen = s.length();
         Character[][] matrix = new Character [numRows][stringLen];
         int stringIterator = 0;
@@ -10,22 +10,19 @@ class Solution {
         int col = 0;
         String res = "";
         boolean incrementRow = false;
-        boolean decrementRow = false;
 
         while (stringIterator != stringLen) {
             matrix[row][col] = s.charAt(stringIterator);
 
             if (row == 0) {
                 incrementRow = true;
-                decrementRow = false;
             } else if (row == numRows - 1) {
-                decrementRow = true;
                 incrementRow = false;
             }
 
             if (incrementRow) {
                 row ++;
-            } else if (decrementRow) {
+            } else {
                 row--;
                 col ++;
             }
