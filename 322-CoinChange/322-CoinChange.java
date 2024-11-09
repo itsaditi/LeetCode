e6c95c58a3d7bcd5f55eaf55017110d8a37e6db4
itsaditi <aditi3049@gmail.com>
2024-11-09 15:17:19 -0500

    Create file

---
+++ 322-CoinChange/322-CoinChange.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] count = new int[amount];
+        return checkCoin(coins, amount, count);
+    }
+
+    private int checkCoin(int[] coins, int amount, int[] count) {
+        if (amount == 0) return 0;
+
+        if (amount < 0) return -1;
+
+        if (count[amount - 1] != 0) return count[amount - 1];
+
+        int leastDiff = Integer.MAX_VALUE;
+
+        for (int cn: coins) {
+            int diff = checkCoin(coins, amount - cn, count);
+            if (diff >= 0 && diff < leastDiff) {
+                leastDiff = 1 + diff;
+            }
+        }
+
+        count[amount - 1] = leastDiff == Integer.MAX_VALUE ? -1 : leastDiff;
+        System.out.println(count[amount - 1]);
+
+        return count[amount - 1];
+    }
+}
