0a4024ec9e1302f67f9b2935e90664e5e1a525c8
itsaditi <aditi3049@gmail.com>
2024-11-09 18:18:38 -0500

    Update file 11/9/2024, 6:18:38 PM

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,36 +1,16 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0]-b[0]);
+        ArrayList<int[]> mergedIntervals = new ArrayList<>();
 
-        // Sort intervals by 1st element
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        LinkedList<int[]> merged = new LinkedList<>();
-        for (int[] interval : intervals) {
-            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
-                merged.add(interval);
-            }
-            else {
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+        for (int[] interval: intervals) {
+            if (mergedIntervals.isEmpty() || mergedIntervals.get(mergedIntervals.size()-1)[1] < interval[0]) {
+                mergedIntervals.add(interval);
+            } else {
+                mergedIntervals.get(mergedIntervals.size()-1)[1] = Math.max(interval[1], mergedIntervals.get(mergedIntervals.size()-1)[1]);
             }
         }
-        return merged.toArray(new int[merged.size()][2]);
 
-        // int[][] merged = new int[intervals.length][2];
-        // int start = 0;
-
-        // for(int[] interval: intervals){
-
-        //     if(start == 0 || merged[start - 1][1] < interval[0]){
-        //         merged[start] = interval;
-        //         start++;
-        //     } else {
-        //         merged[start-1][1] = Math.max(merged[start-1][1], interval[1]);
-        //     }
-        // }
-
-        // return Arrays.copyOf(merged, start);
-        // return merged;
-
-    
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][2]);
     }
 }
