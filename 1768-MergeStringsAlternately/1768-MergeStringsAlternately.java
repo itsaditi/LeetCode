57ef7bb9935da3ad8ffdeedd92d8925d78908982
itsaditi <aditi3049@gmail.com>
2024-12-02 15:18:01 -0500

    Create file

---
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        String res = "";
+        int i = 0, j = 0;
+        char[] word1Arr = word1.toCharArray();
+        char[] word2Arr = word2.toCharArray();
+
+        while (i + j <= word1.length() + word2.length()) {
+            
+            if (i == word1.length()) {
+                System.out.println(word2.substring(j));
+                res += word2.substring(j);
+                break;
+            }
+
+            if (j == word2.length()) {
+                res += word1.substring(i);
+                break;
+            }
+            res += String.valueOf(word1Arr[i]) + String.valueOf(word2Arr[j]);
+
+            i++;
+            j++;
+
+        }
+        return res;
+    }
+}
