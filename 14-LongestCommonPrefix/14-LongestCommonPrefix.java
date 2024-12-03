ea460531cd0ccd05866e8cc8940cfeb77260e723
itsaditi <aditi3049@gmail.com>
2024-12-03 14:17:32 -0500

    Create file

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String res = "";
+
+        int smallestString = Integer.MAX_VALUE;
+
+        for (String str: strs) {
+            smallestString = Math.min(smallestString, str.length());
+        }
+        
+        for (int counter = 0; counter < smallestString ; counter++) {
+            char charAtIndex = strs[0].charAt(counter);
+            int commonFound = 1;
+            int unCommonFound = 1;
+
+            for (int i = 1 ; i < strs.length ; i ++) {
+
+                if (charAtIndex == strs[i].charAt(counter)) {
+                    commonFound++;
+                } else {
+                    unCommonFound++;
+                    break;
+                }
+            }
+            if (commonFound == strs.length) {
+                res += charAtIndex;
+            }
+            if (unCommonFound == strs.length || unCommonFound > 1) {
+                break;
+            }
+        }
+
+        return res;       
+
+    }
+}
