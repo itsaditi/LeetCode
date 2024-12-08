7ba648cbee5f440145200c9d2bb592c8dc478abd
itsaditi <aditi3049@gmail.com>
2024-12-08 16:40:49 -0500

    Update file 12/8/2024, 4:40:49 PM

--- 2054-TwoBestNon-OverlappingEvents/2054-TwoBestNon-OverlappingEvents.java
+++ 2054-TwoBestNon-OverlappingEvents/2054-TwoBestNon-OverlappingEvents.java
@@ -9,10 +9,17 @@ class Solution {
 
         Arrays.sort(events, (a, b) -> a[0] - b[0]);
 
+        // As we process each event, we remove all valid events from the priority queue 
+        // that end before the current event starts, as they are guaranteed not to overlap. 
+        // While removing these events, we update maxVal to store the highest value of 
+        // these popped events. For the current event, we calculate the maximum possible 
+        // sum by adding its value to maxVal, representing the best event that ended before 
+        // the current event started, and update the maxSum if this sum is greater. 
+        // The current event is then added to the priority queue to be considered for future combinations.
 
         for (int[] event: events) {
             
-            // Check if events are overlapping
+            // Pop non-overlapping events
             while(!pq.isEmpty() && pq.peek().getKey() < event[0]) {
                 maxVal = Math.max(maxVal, pq.peek().getValue());
                 pq.poll();
