a7d4cc9f1b8500e9226717bfcf85d893e0b33ade
itsaditi <aditi3049@gmail.com>
2024-12-14 20:35:04 -0500

    Create file

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        if (n == 1 && m == 1) return;
+
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < m ; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.add(new Pair(i, j));
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            Pair<Integer, Integer> poppedElement = queue.poll();
+            int r = poppedElement.getKey();
+            int c = poppedElement.getValue();
+
+            for (int i = 0 ; i <= n + 1; i ++) {
+                matrix[i % n][c] = 0;
+            }
+
+            for (int i = 0 ; i <= m + 1; i ++) {
+                System.out.println(r + " " + i % m + " " + i);
+                matrix[r][i % m] = 0;
+            }
+        }
+    }
+}
