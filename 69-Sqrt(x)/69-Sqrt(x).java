9b3337e92178c520ce4f6c9c025b11d48cd6f6b2
itsaditi <aditi3049@gmail.com>
2024-12-19 00:25:16 -0500

    Update file 12/19/2024, 12:25:15 AM

--- 69-Sqrt(x)/69-Sqrt(x).java
+++ 69-Sqrt(x)/69-Sqrt(x).java
@@ -1,7 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        int sqrt = (int) Math.pow(x, 0.5);
+        // int sqrt = (int) Math.pow(x, 0.5);
+        if (x < 2) return x;
 
-        return sqrt;
+        long num;
+        int left = 0, right = x / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            num = (long) mid * mid;
+            if (num < x) {
+                left = mid + 1;
+            } else if (num > x) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return right;
     }
 }
