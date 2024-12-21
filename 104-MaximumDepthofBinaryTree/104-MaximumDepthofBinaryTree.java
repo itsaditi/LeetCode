48dd52604b296646e6a8664525b2f263a7b0de34
itsaditi <aditi3049@gmail.com>
2024-12-21 13:19:07 -0500

    Update file 12/21/2024, 1:19:07 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -15,7 +15,36 @@
  */
 class Solution {
     int maxDepth = 0;
+
     public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0));
+
+        while (!queue.isEmpty()) {
+            Pair popElem = queue.poll();
+
+            TreeNode node = (TreeNode) popElem.getKey();
+            int level = (int) popElem.getValue();
+
+            if (node.left != null) {
+                queue.offer(new Pair(node.left, level + 1));
+            }
+
+
+            if (node.right != null) {
+                queue.offer(new Pair(node.right, level + 1));
+            }
+
+            maxDepth = Math.max(maxDepth, level);
+        }
+
+        return maxDepth + 1;
+
+    }
+
+
+    public int maxDepth_UsingDFS(TreeNode root) {
         dfs(root, 1);
         return maxDepth;
     }
