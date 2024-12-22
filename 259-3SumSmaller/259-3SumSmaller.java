b293abf1722511e3cf1a82aea4f03cca0cb7738c
itsaditi <aditi3049@gmail.com>
2024-12-22 13:34:13 -0500

    Update file 12/22/2024, 1:34:13 PM

--- 259-3SumSmaller/259-3SumSmaller.java
+++ 259-3SumSmaller/259-3SumSmaller.java
@@ -1,29 +1,38 @@
 class Solution {
     public int threeSumSmaller(int[] nums, int target) {
-        
+        if (nums.length < 3) return 0;
 
+        int numOfTriplets = 0;
         Arrays.sort(nums);
-        int count = 0;
 
-        for(int i = 0 ; i < nums.length - 2 ; i++){
-
-            count+= searchPair(nums, target - nums[i], i);
+        for (int i = 0; i < nums.length - 2; i++) {
+            numOfTriplets += twoSum(nums, i, target);
         }
-        return count;
+
+        return numOfTriplets;
     }
 
-    private int searchPair(int[] nums, int target, int first){
-
+    private int twoSum(
+        int[] nums,
+        int i,
+        int target
+    ) {
+        int left = i + 1;
+        int right = nums.length - 1;
         int count = 0;
-        int start = first + 1;
-        int end = nums.length - 1;
+        while (left < right) {
+            int sum = target - nums[left] - nums[right];
 
-        while(start < end){
-            if(nums[start] + nums[end] < target){
-                count+= end - start;
-                start++;
+            if (nums[i] < sum) {
+                // IMPORTANT
+                // TRICKY PART HERE
+                // IF THREESUM < TARGET, THEN BECAUSE THEE ARRAY IS SORTED
+                // ALL NUMBERS IN BETWEEN WILL ALSO BE LESS OR EQUAL TO K
+                // AND THEREFORE BE VALID ANSWERS
+                count += right - left;
+                left++;
             } else {
-                end--;
+                right--;
             }
         }
         return count;
