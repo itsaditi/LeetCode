d8954c07b739aa16d4bcd5990c16ca6b6b2f1a25
itsaditi <aditi3049@gmail.com>
2025-01-01 18:51:02 -0500

    Create file

---
+++ 1091-ShortestPathinBinaryMatrix/1091-ShortestPathinBinaryMatrix.java
@@ -0,0 +1,61 @@
+class Solution {
+    int[][] DIRECTIONS = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+    int row = 0, col = 0;
+    int shortDistance = Integer.MAX_VALUE;
+
+    // BFS - TLE
+    // TC - O(row  * col) (TC for BFS is O(vertices + edges))
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+
+        row = grid.length;
+        col = grid[0].length;
+
+        if (grid[0][0] == 1 || grid[row - 1][col - 1] == 1) return -1;
+
+        int shortestDistance = Integer.MAX_VALUE;
+
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visitedDP = new boolean[row][col];
+        System.out.println(visitedDP[0][0]);
+
+        // Start BFS from the top-left cell
+        queue.add(new int[]{0, 0, 1});
+        visitedDP[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            int[] poppedEle = queue.poll();
+            int r = poppedEle[0];
+            int c = poppedEle[1];
+            int distance = poppedEle[2];
+    
+            if (r == row - 1 && c == col - 1) {
+                return distance;
+            }
+
+            for (int[] direction: DIRECTIONS) {
+                int neighborRow = r + direction[0];
+                int neighborCol = c + direction[1];
+
+                // if (!isValid(neighborRow, neighborCol)) continue;
+                // if (grid[neighborRow][neighborCol] == 1) continue;
+
+                if (
+                    isValid(neighborRow, neighborCol) &&
+                    grid[neighborRow][neighborCol] == 0 &&
+                    !visitedDP[neighborRow][neighborCol]
+                ) {
+                    queue.add(new int[]{neighborRow, neighborCol, distance + 1});
+                    visitedDP[neighborRow][neighborCol] = true;
+                }
+            }
+            
+        }
+
+        return -1;
+    }
+
+    private boolean isValid(int i, int j) {
+        return (i >= 0 && i < row) && (j >= 0 && j < col);
+    }
+}
