0dd9181ad7a3dead918e093bd2e79910ba553f62
itsaditi <aditi3049@gmail.com>
2025-01-06 17:22:57 -0500

    Update file 1/6/2025, 5:22:57 PM

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,35 +1,22 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
-
-        for(ListNode node: lists) {
-            if (node != null) minHeap.add(node);
+public ListNode mergeKLists(ListNode[] lists) {
+    List<Integer> l = new ArrayList<Integer>();
+   
+    for (ListNode ln : lists) {
+        while (ln != null) {
+            l.add(ln.val);
+            ln = ln.next;
         }
-
-        ListNode head = new ListNode(0);
-        ListNode curr = head;
-
-        while (!minHeap.isEmpty()) {
-            ListNode poppedNode = minHeap.poll();
-
-            curr.next = poppedNode;
-            curr = poppedNode;
-
-            if (poppedNode.next != null) {
-                minHeap.add(poppedNode.next);
-            }
-        }
-
-        return head.next;
     }
+   
+    Collections.sort(l);
+ 
+    ListNode head = new ListNode(0);
+    ListNode h = head;
+    for (int i : l) {
+        ListNode t = new ListNode(i);
+        h.next = t;
+        h = h.next;
+    }
+    h.next = null;
+    return head.next;
 }
