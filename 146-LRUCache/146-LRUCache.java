2b749b2055ff30c33dd68a179a0dff6bce15afcb
itsaditi <aditi3049@gmail.com>
2025-01-06 21:12:22 -0500

    Update file 1/6/2025, 9:12:22 PM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,29 +1,31 @@
 class LRUCache {
 
     HashMap<Integer, Integer> cache;
-    List<Integer> list;
+    Queue<Integer> list;
     int cacheCapacity = 0;
     public LRUCache(int capacity) {
         cache = new HashMap<>();
-        list = new ArrayList<>();
+        list = new LinkedList<>();
         cacheCapacity = capacity;
     }
     
+    // Time Complexity - O(N)
     public int get(int key) {
         if (cache.containsKey(key)) {
-            list.remove(Integer.valueOf(key));
-            list.add(key);
+            list.remove(Integer.valueOf(key)); // O(N)
+            list.add(key); // O(1)
             return cache.get(key);
         }
         return -1;
     }
     
+    // Time Complexity - O(N)
     public void put(int key, int value) {
 
 
         if (cache.size() == cacheCapacity && !cache.containsKey(key)) {
-            cache.remove(list.get(0));
-            list.remove(Integer.valueOf(list.get(0)));
+            cache.remove(list.poll());
+            
         }
 
         if (cache.containsKey(key)) {
@@ -31,7 +33,6 @@ class LRUCache {
         }
         cache.put(key, value);
         list.add(key);
-
     }
 }
 
