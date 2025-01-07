f6ef264224213f66975292361feff65f96f1a0a8
itsaditi <aditi3049@gmail.com>
2025-01-06 23:53:46 -0500

    Create file

---
+++ 1492-ThekthFactorofn/1492-ThekthFactorofn.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        if (n <= 2) return n;
+
+        List<Integer> factors = new ArrayList<>();
+        for (int i = 1; i <= n ; i++) {
+            if (n % i == 0) factors.add(i);
+        }
+
+        if (k >= n || k > factors.size()) return -1;
+
+
+        return factors.get(k - 1);
+    }
+}
