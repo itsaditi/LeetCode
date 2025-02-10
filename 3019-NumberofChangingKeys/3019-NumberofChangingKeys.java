a4faad5e77da411ebe67314eb9d5119432bbcf3e
itsaditi <aditi3049@gmail.com>
2025-02-09 20:46:36 -0500

    Create file

---
+++ 3019-NumberofChangingKeys/3019-NumberofChangingKeys.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countKeyChanges(String s) {
+        s = s.toLowerCase();
+        char ch = s.charAt(0);
+
+        int keyChange = 0;
+
+        for (int i = 1; i < s.length() ; i++) {
+            if (ch != s.charAt(i)) {
+                keyChange++;
+                ch = s.charAt(i);
+            }
+        }
+
+        return keyChange;
+    }
+}
