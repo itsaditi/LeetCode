482bc3da4c6315f2e1b25dc2129ecc6349f77ba9
itsaditi <aditi3049@gmail.com>
2025-02-09 20:41:13 -0500

    Create file

---
+++ 3042-CountPrefixandSuffixPairsI/3042-CountPrefixandSuffixPairsI.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countPrefixSuffixPairs(String[] words) {
+        
+        int indexPairs = 0;
+
+        for (int i = 0 ; i < words.length - 1 ; i++) {
+            for (int j = i + 1 ; j < words.length ; j++) {
+                if (isPrefixAndSuffix(words[i], words[j])) {
+                    indexPairs++;
+                }
+            } 
+        }
+
+
+
+        return indexPairs;
+    }
+    private boolean isPrefixAndSuffix(String str1, String str2) {
+        return (str2.startsWith(str1) && str2.endsWith(str1));
+    }
+}
