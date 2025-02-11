6674949ed1ba274268f5fad46f1beabf232c4595
itsaditi <aditi3049@gmail.com>
2025-02-10 19:50:01 -0500

    File updated at 2/10/2025, 7:50:01 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,29 +1,93 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1 ;
+        int n = nums.length;
+        int left = 0, right = n - 1;
 
-        while (start <= end) {
-            int midIndex = (end + start) / 2;
+        // Find the index of the pivot element (the smallest element)
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[n - 1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
 
-            if (nums[midIndex] == target) return midIndex;
-            else {
-                if (nums[midIndex] >= nums[start]) {
-                    if (target >= nums[start] && target < nums[midIndex]) {
-                        end = midIndex - 1;
-                    } else {
-                        start = midIndex + 1;
-                    }
-                } else {
-                    if (target <= nums[end] && target > nums[midIndex]) {
-                        start = midIndex + 1;
-                    } else {
-                        end = midIndex - 1;
-                    }
-                }
+        // Binary search over elements on the pivot element's left
+        int answer = binarySearch(nums, 0, left - 1, target);
+        if (answer != -1) {
+            return answer;
+        }
+
+        // Binary search over elements on the pivot element's right
+        return binarySearch(nums, left, n - 1, target);
+    }
+
+    // Binary search over an inclusive range [left_boundary ~ right_boundary]
+    private int binarySearch(
+        int[] nums,
+        int leftBoundary,
+        int rightBoundary,
+        int target
+    ) {
+        int left = leftBoundary, right = rightBoundary;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+
+class Solution2 {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] < nums[n - 1]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        int leftHalf = binarySearch(nums, 0, left - 1, target);
+
+        if (leftHalf != -1) {
+            return leftHalf;
+        } 
+        
+        return binarySearch(nums, left, nums.length - 1, target);
+    }
+    // 0,1,2,3,4,5 
+    private int binarySearch(int[] nums, int startIndex, int endIndex, int target) {
+        while (startIndex <= endIndex) {
+            int mid = (startIndex + endIndex) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                startIndex = mid + 1;
+            } else {
+                endIndex = mid - 1;
             }
         }
 
         return -1;
     }
 }
+
+
+// 1,2,3,4,5,6,7,0
+// 6,7,0,1,2,3,4,5
+// 4,5,6,7,0,1,2
