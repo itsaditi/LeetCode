309153581cd97cc19c5a0cb7e924c9e104f410c8
itsaditi <aditi3049@gmail.com>
2025-07-20 21:44:00 -0400

    Create 1436-DestinationCity

---
+++ 1436-DestinationCity/1436-DestinationCity.java
@@ -0,0 +1,27 @@
+// Last updated: 7/20/2025, 9:44:00 PM
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        HashMap<String, String> map = new HashMap<>();
+
+        for (List<String> path: paths) {
+            if (!map.containsKey(path.get(0))) {
+                map.put(path.get(0), null);
+            }
+
+            if (!map.containsKey(path.get(1))) {
+                map.put(path.get(1), null);
+            }
+
+            map.put(path.get(0), path.get(1));
+        }
+
+        for (Map.Entry<String, String> mapSet: map.entrySet()) {
+            if (mapSet.getValue() == null) {
+                return mapSet.getKey();
+            }
+        }
+
+
+        return "";
+    }
+}
