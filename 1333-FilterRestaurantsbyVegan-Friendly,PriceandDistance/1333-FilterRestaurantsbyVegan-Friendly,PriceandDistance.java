40ad9decf6c750f8da6b1980c4a3a1888f510246
itsaditi <aditi3049@gmail.com>
2025-07-20 20:15:04 -0400

    Create 1333-FilterRestaurantsbyVegan-Friendly,PriceandDistance

---
+++ 1333-FilterRestaurantsbyVegan-Friendly,PriceandDistance/1333-FilterRestaurantsbyVegan-Friendly,PriceandDistance.java
@@ -0,0 +1,40 @@
+// Last updated: 7/20/2025, 8:15:04 PM
+class Solution {
+    public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
+        List<int[]> tempResult = new ArrayList<>();
+
+        for (int[] info: restaurants) {
+            int id = info[0];
+            int rating = info[1];
+            int veganFri = info[2];
+            int price = info[3];
+            int distance = info[4];
+            if (price <= maxPrice && distance <= maxDistance) {
+                if (veganFriendly == 1 ) {
+                    if (veganFri == 1)
+                        tempResult.add(new int[]{id, rating});
+                } else {
+                    tempResult.add(new int[]{id, rating});
+                }
+            }
+        }
+
+        tempResult.sort((a,b) -> {
+            if (a[1] != b[1]) {
+                return Integer.compare(a[1], b[1]);
+            } else {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+        Collections.reverse(tempResult);
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int[] id: tempResult) {
+            result.add(id[0]);
+        }
+
+        return result;
+
+    }
+}
