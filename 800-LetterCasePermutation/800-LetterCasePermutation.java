5e25c1b91195a777963fcaf4bd3cd682663f9e6c
itsaditi <aditi3049@gmail.com>
2025-08-04 16:50:53 -0400

    Create 800-LetterCasePermutation

---
+++ 800-LetterCasePermutation/800-LetterCasePermutation.java
@@ -0,0 +1,34 @@
+// Last updated: 8/4/2025, 4:50:52 PM
+class Solution {
+    List<String> res = new ArrayList<>();
+    int n = 0;
+
+    public List<String> letterCasePermutation(String s) {
+        n = s.length();
+        backTracking(0, "", s);
+        return res;
+    }
+
+    private void backTracking(int index, String str, String s) {
+        if (str.length() == n) {
+            res.add(str);
+            return;
+        }
+        
+        if (index > n - 1) return;
+
+        char ch = s.charAt(index);
+        index++;
+
+        if (Character.isDigit(ch)) {
+            String newString = str + String.valueOf(ch);
+            backTracking(index, newString, s);
+        } else {
+            String lowerCase = str + String.valueOf(ch).toLowerCase();
+            backTracking(index, lowerCase, s);
+
+            String upperCase = str + String.valueOf(ch).toUpperCase();
+            backTracking(index, upperCase, s);
+        }
+    }
+}
