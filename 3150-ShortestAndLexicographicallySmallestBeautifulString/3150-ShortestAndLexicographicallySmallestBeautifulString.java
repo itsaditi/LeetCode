471346a62c510c2e2c023e0f4521a20bfd8ea336
itsaditi <aditi3049@gmail.com>
2025-08-04 16:50:12 -0400

    Create 3150-ShortestAndLexicographicallySmallestBeautifulString

---
+++ 3150-ShortestAndLexicographicallySmallestBeautifulString/3150-ShortestAndLexicographicallySmallestBeautifulString.java
@@ -0,0 +1,40 @@
+// Last updated: 8/4/2025, 4:50:12 PM
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        
+        int[] prefixSum = new int[s.length()];
+
+        // Compute prefix sum 
+        int numOfOnes = 0;
+        for (int i = 0 ; i < s.length() ; i++) {
+            if (s.charAt(i) == '1') {
+                numOfOnes ++;
+            };
+            prefixSum[i] = numOfOnes;
+        }
+
+        if (numOfOnes < k) return "";
+
+        if (k == 1 && numOfOnes >= k) return "1";
+
+        String beautString = null;
+
+        for (int l = 0 ; l < s.length(); l++) {
+            for (int r = l + 1; r < s.length() ; r++) {
+                int countOnes = (l == 0) ? prefixSum[r] : (prefixSum[r] - prefixSum[l - 1]); 
+                if (countOnes == k) {
+                    String candidate = s.substring(l, r + 1);
+                    if (
+                        beautString == null ||
+                        candidate.length() < beautString.length() || 
+                        (candidate.length() == beautString.length() && candidate.compareTo(beautString) < 0)
+                    ) {
+                            beautString = candidate;
+                    }
+                } 
+                
+            }
+        }
+        return beautString;
+    }
+}
