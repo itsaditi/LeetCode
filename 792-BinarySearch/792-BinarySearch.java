1fda1e0ce8d93715e84e072191d6eb5fa7c22cae
itsaditi <aditi3049@gmail.com>
2025-08-04 16:50:53 -0400

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,40 @@
+// Last updated: 8/4/2025, 4:50:53 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        // Edge case
+        if (nums.length == 1 && nums[0] == target) return 0;
+
+        int startIndex = 0, endIndex = nums.length - 1;
+        int result = -1;
+
+        // Sliding Window
+        while(startIndex <= endIndex) {
+            int midIndex = (startIndex + endIndex) / 2;
+
+            if (nums[midIndex] == target) {
+                result = midIndex;
+                break;
+            } else {
+                if (target < nums[midIndex]) {
+                    endIndex = midIndex - 1;
+                    if (endIndex >= 0 && target == nums[endIndex]) {
+                        result = endIndex;
+                        break;
+                    }
+                } else if (target > nums[midIndex]) {
+                    startIndex = midIndex + 1;
+                    if (startIndex <= nums.length - 1 && target == nums[startIndex]) {
+                        result = startIndex;
+                        break;
+                    }
+                }
+            }
+
+            if (endIndex < 0) break;
+            if (startIndex > nums.length - 1) break;
+        }
+
+        return result;
+        
+    }
+}
