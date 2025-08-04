caed73c7c8e00345075dad89d2fdfcea4a237302
itsaditi <aditi3049@gmail.com>
2025-08-04 16:50:39 -0400

    Create 1029-VerticalOrderTraversalOfABinaryTree

---
+++ 1029-VerticalOrderTraversalOfABinaryTree/1029-VerticalOrderTraversalOfABinaryTree.java
@@ -0,0 +1,60 @@
+// Last updated: 8/4/2025, 4:50:39 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ // FIX THE
+class Solution {
+
+    // Time Complexity - O(NLogN)
+    // Space Complexity - O(N)
+    TreeMap<Integer, TreeMap<Integer, List<Integer>>> map = new TreeMap<>();
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(0,0, root);
+
+        for (Integer key: map.keySet()) {
+            // Get Column
+            TreeMap<Integer, List<Integer>> treeMap = map.get(key);
+            List<Integer> res = new ArrayList<>();
+
+            for (Integer treeMapKey: treeMap.keySet()) {
+                List<Integer> ls = treeMap.get(treeMapKey);
+                Collections.sort(ls);
+                res.addAll(ls);
+            }
+            result.add(res);
+
+        }
+        return result;
+    }
+
+    private void dfs(int row, int col, TreeNode root) {
+        if (root == null) return;
+
+        if (!map.containsKey(col)) {
+            map.put(col, new TreeMap<>());
+        }
+
+        if (!map.get(col).containsKey(row)) {
+            map.get(col).put(row, new ArrayList<>());
+        }
+
+        map.get(col).get(row).add(root.val);
+
+        dfs(row + 1, col - 1, root.left);
+        dfs(row + 1, col + 1, root.right);
+    }
+}
