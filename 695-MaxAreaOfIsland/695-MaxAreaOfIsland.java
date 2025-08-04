1ec44ae08b46ccba323fc844ec3468d999b2c376
itsaditi <aditi3049@gmail.com>
2025-08-04 16:50:58 -0400

    Create 695-MaxAreaOfIsland

---
+++ 695-MaxAreaOfIsland/695-MaxAreaOfIsland.java
@@ -0,0 +1,39 @@
+// Last updated: 8/4/2025, 4:50:58 PM
+class Solution {
+    int maxArea = 0;
+    int area = 0;
+    int row = 0, col = 0;
+    public int maxAreaOfIsland(int[][] grid) {
+        row = grid.length;
+        col = grid[0].length;
+        for (int i = 0 ; i < grid.length ; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    area = 0;
+                    bfs(grid, i, j);
+                    maxArea = Math.max(area, maxArea);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private void bfs(int[][] grid, int i, int j) {
+        if ((i < 0 || i >= row) || (j < 0 || j >= col)) return;
+
+        if (grid[i][j] == 0) return;
+
+        if (grid[i][j] == 1){
+            grid[i][j] = 0;
+            area++;
+        }
+        
+
+        bfs(grid, i + 1, j);
+        bfs(grid, i, j + 1);
+        bfs(grid, i - 1, j);
+        bfs(grid, i, j - 1);
+
+        return;
+    }
+}
