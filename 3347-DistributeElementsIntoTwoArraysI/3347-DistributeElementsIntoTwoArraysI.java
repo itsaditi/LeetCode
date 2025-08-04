6d53d5767f2ec5c3b8ba6f073b65c808ca5a3ca7
itsaditi <aditi3049@gmail.com>
2025-08-04 16:50:09 -0400

    Create 3347-DistributeElementsIntoTwoArraysI

---
+++ 3347-DistributeElementsIntoTwoArraysI/3347-DistributeElementsIntoTwoArraysI.java
@@ -0,0 +1,38 @@
+// Last updated: 8/4/2025, 4:50:09 PM
+class Solution {
+    public int[] resultArray(int[] nums) {
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+
+        arr1.add(nums[0]);
+        arr2.add(nums[1]);
+
+        for (int i = 2; i < nums.length ; i++) {
+            if (arr1.getLast() > arr2.getLast()) {
+                arr1.add(nums[i]);
+            } else {
+                arr2.add(nums[i]);
+            }
+        }
+
+        // int[] resultArr = new int[arr1.size() + arr2.size()];
+        // int index = 0;
+
+        // for (int num: arr1) {
+        //     resultArr[index] = num;
+        //     index++;
+        // }
+        
+        // for (int num: arr2) {
+        //     resultArr[index] = num;
+        //     index++;
+        // }
+
+        arr1.addAll(arr2);
+        
+
+
+        return arr1.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+}
