b6535ea71b6c33a41aeda4fb1aede8b5590a4c55
itsaditi <aditi3049@gmail.com>
2025-08-04 16:50:18 -0400

    Create 2800-MinimumStringLengthAfterRemovingSubstrings

---
+++ 2800-MinimumStringLengthAfterRemovingSubstrings/2800-MinimumStringLengthAfterRemovingSubstrings.java
@@ -0,0 +1,27 @@
+// Last updated: 8/4/2025, 4:50:17 PM
+class Solution {
+    public int minLength(String s) {
+        String str = minString(s);;
+        
+        return str.length();
+        
+    }
+
+    private String minString(String str) {
+        // Base case
+        if (str.length() == 0) return "";
+
+        int abIndex = str.indexOf("AB");
+        int cdIndex = str.indexOf("CD");
+
+        if (abIndex == -1 && cdIndex == -1) return str;
+
+        if (abIndex > -1) {
+            str = str.replaceFirst(str.substring(abIndex, abIndex + 2), "");
+        } else if (cdIndex > -1) {
+            str = str.replaceFirst(str.substring(cdIndex, cdIndex + 2), "");
+        }
+
+        return minString(str);
+    }
+}
