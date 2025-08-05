06dd270df7a9cdc9e9b070871726d4817b344151
itsaditi <aditi3049@gmail.com>
2025-08-04 23:13:14 -0400

    Create 2775-UndefinedtoNull

---
+++ 2775-UndefinedtoNull/2775-UndefinedtoNull.js
@@ -0,0 +1,23 @@
+// Last updated: 8/4/2025, 11:13:13 PM
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var undefinedToNull = function(obj) {
+    for (const key in obj) {
+        if (Array.isArray(obj[key]) || typeof obj[key] === 'object') {
+            undefinedToNull(obj[key]);
+        } else {
+            if (obj[key] === undefined) {
+                obj[key] = null;
+            }
+        }
+    }
+
+    return obj;
+};
+
+/**
+ * undefinedToNull({"a": undefined, "b": 3}) // {"a": null, "b": 3}
+ * undefinedToNull([undefined, undefined]) // [null, null] 
+ */
