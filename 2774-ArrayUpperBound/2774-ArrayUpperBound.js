a56a69bb1d43c9ef6b0d653dfe5bc2276c1b4689
itsaditi <aditi3049@gmail.com>
2025-08-04 23:06:57 -0400

    File updated at 8/4/2025, 11:06:57 PM

--- 2774-ArrayUpperBound/2774-ArrayUpperBound.js
+++ 2774-ArrayUpperBound/2774-ArrayUpperBound.js
@@ -1,21 +1,52 @@
-// Last updated: 8/4/2025, 10:59:45 PM
+// Last updated: 8/4/2025, 11:06:57 PM
 /** 
  * @param {number} target
  * @return {number}
  */
 
+// Approach 1 
+// Array.prototype.upperBound = function(target) {
 
+//     const map = new Map();
+
+//     for (let i = 0 ; i < this.length ; i ++) {
+//         map.set(this[i], i);
+//     }
+
+//     return map.has(target) ? map.get(target) : -1;
+// };
+
+// Approach 2 - Inbuilt function
+// Array.prototype.upperBound = function(target) {
+//     return this.lastIndexOf(target);
+// };
+
+// Approach 3 - Binary search
 Array.prototype.upperBound = function(target) {
+    let left = 0;
+    let right = this.length - 1;
 
-    const map = new Map();
+    let result = -1;
 
-    for (let i = 0 ; i < this.length ; i ++) {
-        map.set(this[i], i);
+    while (left <= right) {
+        let mid = left + ((right - left) >> 1);
+
+        if (this[mid] === target) {
+            result = mid;
+            left = mid + 1
+        } else if (this[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1;
+        }
+
     }
 
-    return map.has(target) ? map.get(target) : -1;
-};
+    return result;
 
+
+
+}
 
 // [3,4,5].upperBound(5); // 2
 // [1,4,5].upperBound(2); // -1
