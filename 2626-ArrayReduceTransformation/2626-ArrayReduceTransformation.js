58484d2c3012f3d294c19b3ea33d9f2aa4585a46
itsaditi <aditi3049@gmail.com>
2025-08-05 11:46:47 -0400

    Create 2626-ArrayReduceTransformation

---
+++ 2626-ArrayReduceTransformation/2626-ArrayReduceTransformation.js
@@ -0,0 +1,19 @@
+// Last updated: 8/5/2025, 11:46:46 AM
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    if (nums.length === 0) return init;
+
+    let val = 0;
+
+    for (let i = 0 ; i < nums.length ; i++) {
+        val = fn(i === 0 ? init : val, nums[i]);
+        console.log(val);
+    }
+
+    return val;
+};
