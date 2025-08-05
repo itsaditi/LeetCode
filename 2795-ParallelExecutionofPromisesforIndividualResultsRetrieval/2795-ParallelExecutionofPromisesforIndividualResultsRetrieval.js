635778545f9f93ace4f3a4a1b54f85978e52e679
itsaditi <aditi3049@gmail.com>
2025-08-04 22:22:41 -0400

    Create 2795-ParallelExecutionofPromisesforIndividualResultsRetrieval

---
+++ 2795-ParallelExecutionofPromisesforIndividualResultsRetrieval/2795-ParallelExecutionofPromisesforIndividualResultsRetrieval.js
@@ -0,0 +1,42 @@
+// Last updated: 8/4/2025, 10:22:40 PM
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<Array>}
+ */
+var promiseAllSettled = function(functions) {
+
+    return new Promise((resolve, reject) => {
+        const res = new Array(functions.length).fill(null);
+        let settledCounter = 0;
+
+        functions.forEach((func, index) => {
+            func().then((response) => {
+                res[index] = ({status: "fulfilled", value: response});
+            }).catch((err) => {
+                res[index] = ({status: "rejected", reason: err});
+            }).finally(() => {
+                settledCounter++;
+
+                if(settledCounter === functions.length) {
+                    resolve(res);
+                }
+            });
+        })
+    });
+
+};
+
+
+/**
+ * const functions = [
+ *    () => new Promise(resolve => setTimeout(() => resolve(15), 100))
+ * ]
+ * const time = performance.now()
+ *
+ * const promise = promiseAllSettled(functions);
+ *              
+ * promise.then(res => {
+ *     const out = {t: Math.floor(performance.now() - time), values: res}
+ *     console.log(out) // {"t":100,"values":[{"status":"fulfilled","value":15}]}
+ * })
+ */
