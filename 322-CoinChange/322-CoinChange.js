a041b628c9ff1966b89e3bc8d818f12cab5d4cdd
itsaditi <aditi3049@gmail.com>
2025-08-13 19:34:01 -0400

    Create 322-CoinChange

---
+++ 322-CoinChange/322-CoinChange.js
@@ -0,0 +1,11 @@
+// Last updated: 8/13/2025, 7:34:01 PM
+//Example:
+coinChange([1, 2, 5], 7)
+
+// We start with this array:
+[
+  0, Infinity,
+  Infinity, Infinity,
+  Infinity, Infinity,
+  Infinity, Infinity
+]
