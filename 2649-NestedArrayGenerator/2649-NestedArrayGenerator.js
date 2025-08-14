f80402f5b93524a6fd30b6d416f75134881d9678
itsaditi <aditi3049@gmail.com>
2025-08-14 05:19:37 -0400

    Create 2649-NestedArrayGenerator

---
+++ 2649-NestedArrayGenerator/2649-NestedArrayGenerator.js
@@ -0,0 +1,22 @@
+// Last updated: 8/14/2025, 5:19:37 AM
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function*(arr) {
+
+    for (let elem of arr) {
+        if (Array.isArray(elem)) {
+            yield* inorderTraversal(elem);
+        } else {
+            yield elem;
+        }
+    }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
