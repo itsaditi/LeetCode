2453ec0c5ac28fababac04c178f1c17f7e2c2ef1
itsaditi <aditi3049@gmail.com>
2025-10-27 21:32:38 -0400

    Create 2861-CreateObjectFromTwoArrays

---
+++ 2861-CreateObjectFromTwoArrays/2861-CreateObjectFromTwoArrays.js
@@ -0,0 +1,23 @@
+// Last updated: 10/27/2025, 9:32:37 PM
+/**
+ * @param {Array} keysArr
+ * @param {Array} valuesArr
+ * @return {Object}
+ */
+var createObject = function(keysArr, valuesArr) {
+    const newObj = {}
+
+
+    for(let i = 0 ; i < keysArr.length ; i++) {
+        const key = new String(keysArr[i]);
+        const val = valuesArr[i];
+        if (key in newObj) {
+            continue;
+        }
+
+        newObj[key] = val;
+    }
+
+    return newObj;
+
+};
