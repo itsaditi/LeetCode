da108223217ccdcefe61f8a24468b837441a8b0a
itsaditi <aditi3049@gmail.com>
2025-10-27 21:32:38 -0400

    Create 2822-NextDay

---
+++ 2822-NextDay/2822-NextDay.js
@@ -0,0 +1,19 @@
+// Last updated: 10/27/2025, 9:32:38 PM
+/** 
+ * @return {string}
+ */
+Date.prototype.nextDay = function() {
+    const newDate = new Date(this.setDate(this.getDate() + 1));
+    const year = newDate.getFullYear();
+    const month = String(newDate.getMonth() + 1).padStart(2, '0');
+    const day = String(newDate.getDate()).padStart(2, '0');
+    const formatted = `${year}-${month}-${day}`;
+
+    // return newDate;
+    return formatted;
+}
+
+/**
+ * const date = new Date("2014-06-20");
+ * date.nextDay(); // "2014-06-21"
+ */
