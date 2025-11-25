f90c2982c107ac96055e53914d75ea31f5f56f0c
itsaditi <aditi3049@gmail.com>
2025-11-24 21:39:09 -0500

    Create 57-InsertInterval

---
+++ 57-InsertInterval/version/java/57-InsertInterval_20251124_213909.java
@@ -0,0 +1,34 @@
+// Last updated: 11/24/2025, 9:39:09 PM
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> intervalList = new ArrayList<>();
+        boolean newIntervalInserted = false;
+        for (int[] interval: intervals) {
+            if (newInterval[0] < interval[0]) {
+                intervalList.add(newInterval);
+                newIntervalInserted = true;
+            }
+            intervalList.add(interval);
+        }
+
+        if (!newIntervalInserted) intervalList.add(newInterval);
+       
+        // Sort - O(log n)
+        // Collections.sort(intervalList, (a, b) -> a[0] - b[0]);
+
+        List<int[]> resultInterval = new ArrayList<>();
+
+        // [[1,3],[2,5],[6,9]]
+        for (int[] interval: intervalList) {
+            if (resultInterval.isEmpty() || resultInterval.getLast()[1] < interval[0]) {
+                resultInterval.add(interval); // [[1,5], [6,9]]
+            } else {
+                // [[1,3]]
+                resultInterval.getLast()[1] = Math.max(resultInterval.getLast()[1], interval[1]);
+            }
+        }
+
+        return resultInterval.toArray(new int[resultInterval.size()][2]);
+        
+    }
+}
