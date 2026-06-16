f3c6582d7dc8b7e708aa0b78cf18dc39a84805e6
Aditi P <aditi3049@gmail.com>
2026-06-15 22:35:46 -0400

    Create 127-WordLadder

---
+++ 127-WordLadder/version/python/127-WordLadder_20260615_223546.py
@@ -0,0 +1,18 @@
+# Last updated: 6/15/2026, 10:35:46 PM
+adj = collections.defaultdict(list)
+for word in wordList:
+    for i in range(len(word)):
+        adj[word[:i] + '_' + word[i+1:]].append(word)
+visited = set()
+q = collections.deque([(beginWord, 1)])
+while q:
+    word, k = q.popleft()
+    if word == endWord:
+        return k
+    if word not in visited:
+        visited.add(word)
+        for i in range(len(word)):
+            neighbors = word[:i] + '_' + word[i+1:]
+            for neighbor in adj[neighbors]:
+                q.append((neighbor, k+1))
+return 0
